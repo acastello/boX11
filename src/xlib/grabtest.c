fa4138720fc77c76c099651cf93023a479ac0108
Castello <a.castello.pas@gmail.com>
2016-08-24 17:22:03 +0200

    t400

--- src/xlib/grabtest.c
+++ src/xlib/grabtest.c
@@ -5,6 +5,31 @@
 
 int main(void)
 {
-    printf("%p\n", XGrabKey);
+    XEvent ret;
+    XKeyEvent *kev;
+    Display *dpy = XOpenDisplay(NULL);
+    Window root = DefaultRootWindow(dpy);
+
+    XGrabKey(dpy, 32, AnyModifier, root, 1, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, 24, 0x10, root, 1, GrabModeAsync, GrabModeAsync);
+
+    while (1) {
+        XNextEvent(dpy, &ret);
+        printf("type: %d\n", ret.type); 
+
+        kev = (XKeyEvent *) &ret;
+        switch (kev->keycode) {
+            case 24:
+                goto exit;
+                break;
+            default:
+                break;
+        }
+    }
+
+exit:
+    XUngrabKey(dpy, 32, AnyModifier, root);
+    XCloseDisplay(dpy);
+    
     return 0;
 }
