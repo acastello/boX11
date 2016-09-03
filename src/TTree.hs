32931ed7752da4d441954a15df8437c96c8e84ff
ACastello <a.castello.pas@gmail.com>
2016-09-03 20:26:08 +0200

    .

--- src/TTree.hs
+++ src/TTree.hs
@@ -1,12 +1,21 @@
 module TTree where
 
 data TTree a b = TNode a b | TForest a [TTree a b]
-    deriving Show
+    deriving (Show, Eq)
 
-drawTTree ttree = unlines $ draw' ttree where
-    draw' (TNode i x) = concat [show i, " -> ", show x] : []
-    draw' (TForest i xs) = shift (show i ++ " +- ") " |- " (head xs)
+drawTTree :: (Show a, Show b) => TTree a b -> String
+drawTTree ttree = unlines' $ draw' ttree where
+    draw' (TNode i x) = concat [show i, "───{", show x, "}"] : []
+    draw' (TForest i []) = [show i]
+    draw' (TForest i [x]) = shift (show i ++ "───") ((show i *> " ") ++ "   ") (draw' x)
+    draw' (TForest i (x:xs)) = (shift (show i ++ "─┬─") ((show i *> " ") ++ " │ ") (draw' x)) ++ (draw'' (show i *> " ") xs)
+    draw'' p [x] = shift (p ++ " └─") (p ++ "   ") (draw' x)
+    draw'' p (x:xs) = (shift (p ++ " ├─") (p ++ " │ ") (draw' x)) ++ (draw'' p xs)
     shift first other = zipWith (++) (first : repeat other)
+    unlines' [x] = x
+    unlines' [] = []
+    unlines' (x:xs) = x ++ ('\n':(unlines' xs))
+
 instance Functor (TTree a) where
     fmap = fmapTTree
 
@@ -22,6 +31,11 @@ seqTTree (TNode a f) ttree = fmap f ttree
 seqTTree ftree (TNode i e) = fmap ($ e) ftree
 seqTTree ftree (TForest i xs) = TForest i (fmap (seqTTree ftree) xs)
 
+-- instance Foldable (TTree a) where
+    -- foldr = foldrT
+
+foldrT f x (TNode _ y) = f y x
+foldrT f x (TForest _ ys) = foldr (foldrT f) x ys
 
 instance Show (IO a) where
     show _ = "()"
@@ -29,4 +43,4 @@ type Binds = TTree Int (IO ())
 
 u = TNode "u0" 1
 v = TForest "v0" [TNode "v00" 2, TNode "v01" 3]
-w = TForest "w0" [TForest "w00" [TNode "000" 4, TNode "001" 5], TNode "w01" 6, TForest "w02" [TForest "w020" [TNode "w0200" 7]]]
+w = TForest "w0" [TForest "w00" [TNode "000" 4, TNode "001" 5], TNode "w01" 6, TForest "w02" [TForest "w020" [TNode "w0200" 7]], TForest "w03" []]
