84a603bd74fa1a0430144676a43d9c1e4cb282fd
acastello <a.castello.pas@gmail.com>
2016-10-02 13:16:16 +0200

    .

--- src/boxghc/Main.hs
+++ src/boxghc/Main.hs
@@ -1,5 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import System.Process
 import System.Environment
+import System.Posix
+
+import Control.Monad (when)
+
+import qualified Data.ByteString.Lazy as BS
+import Text.Parse.ByteString
+
+exportLines = "foreign export ccall \"box_main\" main :: IO ()"
 
 main = do
     let compiler = "ghc"
@@ -10,3 +20,13 @@ main = do
     print =<< readProcess "ls" [rts] ""
     return ()
     
+fixFile :: FilePath -> IO ()
+fixFile f = do
+    sz <- return . fileSize =<< getFileStatus f
+    print sz
+
+    BS.appendFile f exportLines
+    print =<< BS.readFile f
+    setFileSize f sz
+    return ()
+
