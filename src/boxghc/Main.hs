2b1d4c36f6ef0cab2b7d805840eeea17a817b47f
acastello <a.castello.pas@gmail.com>
2016-10-03 19:09:16 +0200

    .

--- src/boxghc/Main.hs
+++ src/boxghc/Main.hs
@@ -16,20 +16,19 @@ exportLines = "-- lines added by boxghc, delete these if you can read them after
 main = do
     let compiler = "ghc"
     args <- getArgs
-    let target = if args == [] then "Main.hs" else head args
+    let target = if args == [] then error "no target" else head args
     vers <- return . (filter (/= '\n')) =<< readProcess compiler ["--numeric-version"] "" 
     let rts = "/usr/lib/ghc-" ++ vers ++ "/rts/libHSrts-ghc" ++ vers ++ ".so"
     fe <- fileExist rts
     when (not fe) $ error $ "couldn't find runtime library " ++ rts
-    print [show args , vers, rts]
     sz <- return . fileSize =<< getFileStatus target
     finally 
         (do
             BS.appendFile target exportLines 
-            waitForProcess =<< runProcess compiler (args ++ ["-dynamic", "-shared", "-threaded", "-fPIC", "-lboX11", rts]) Nothing Nothing Nothing Nothing Nothing
+            let cmd = args ++ ["-dynamic", "-shared", "-threaded", "-fPIC", "-lboX11", rts]
+            when (not $ elem "-v0" cmd) $ putStrLn $ foldr (\a b -> a ++ ' ':b) compiler cmd
+            waitForProcess =<< runProcess compiler cmd Nothing Nothing Nothing Nothing Nothing
         )
         (do
             setFileSize target sz )
     return ()
-
-foreign export ccall "box_main" main :: IO ()
