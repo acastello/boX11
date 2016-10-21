85f4d6168c11de4067a39676af1ff4ed11237f0d
acastello <a.castello.pas@gmail.com>
2016-10-21 10:38:44 +0200

    .

--- tests/Main9.hs
+++ tests/Main9.hs
@@ -4,12 +4,14 @@ import BoX11.X
 import Data.NMap
 
 keys :: [KM]
-keys = [read "Return"] ++ (enumFromTo (read "scroll-a") (read "scroll-z"))
+keys = [read "Return"] ++ (enumFromTo (read "a") (read "z")) ++ (enumFromTo (read "0") (read "9"))
+
+keysp 
 
 binds :: Traversable t => t HWND -> X Bindings
 binds ws = sequence $ fromList [a .> ($ ws) <$> (broadcast a) | a <- keys]
 main = runX' $ do
-    wins <- getWins byClassEx "GxWindow.*"
+    wins <- getWins byClassEx "GxWindowClass.*"
     io $ print wins
     binds' <- binds wins
     setBindings $ insert0 (read "q") exitX binds'
